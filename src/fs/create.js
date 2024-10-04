6a8fe22b2821a2f85ba41876438d51a32f27abbb
olenaweb <olena.nevzorova@gmail.com>
2024-10-05 02:16:45 +0300

    feat: fs module: create file done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,34 @@
-const create = async () => {
-    // Write your code here 
+// create.js - implement function that creates new file fresh.txt with content
+//  I am fresh and young inside of the files folder 
+//  (if file already exists Error with message FS operation failed must be thrown)
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const pathFile = path.join(_dirname, 'files', 'fresh.txt');
+const content = 'I am fresh and young';
+
+const checkFile = async (file) => {
+  try {
+    await fs.access(file);
+    return true;
+  } catch {
+    return false;
+  }
 };
 
-await create();
+const create = async () => {
+  const isFileExists = await checkFile(pathFile);
+
+  if (isFileExists) {
+    throw new Error('*** FS operation failed. File already exists: ' + pathFile);
+  }
+
+  await fs.writeFile(pathFile, content);
+  console.log(`*** File created: ${pathFile}`);
+};
+
+create().catch((err) => console.error(err.message));
