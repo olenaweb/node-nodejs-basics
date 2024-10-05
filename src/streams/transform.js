37a384928fdf2ab3c2c400ccc6dd8a9d2b38c138
olenaweb <olena.nevzorova@gmail.com>
2024-10-06 02:18:18 +0300

    feat: transform stream done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+// transform.js - implement function that reads data from process.stdin,
+//  reverses text using Transform Stream and then writes it into process.stdout
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  process.stdout.write('Enter your information below. To complete the entry, enter : Ctrl-C.\n ');
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedChunk = chunk.toString().split('').reverse().join('');
+      this.push(reversedChunk);
+      callback();
+    }
+  });
+  process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
+
