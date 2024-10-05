100a8f2c5643fccff2a1e059a6b43728804d5579
olenaweb <olena.nevzorova@gmail.com>
2024-10-05 22:26:56 +0300

    feat: Command line interface(src/cli) done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+// env.js - implement function that parses environment variables with prefix RSS_
+//  and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+// npx cross-env SOME=any RSS_foo=bar RSS_bar=baz node src/cli/env.js
+
 const parseEnv = () => {
-    // Write your code here 
+  const envs = process.env;
+  console.log('*** Environment variables with prefix RSS_ ');
+  for (const name in envs) {
+    const namePart = name.split('_');
+    if (namePart[0] === 'RSS') { console.log(`${name} =  ${envs[name]};`); }
+  }
 };
 
 parseEnv();
