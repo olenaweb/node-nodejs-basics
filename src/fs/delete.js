f5e24b3d0695e8b7835e4068256264f5f5ac51f0
olenaweb <olena.nevzorova@gmail.com>
2024-10-05 15:53:14 +0300

    feat: fs module: module 'remove file' done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,34 @@
+// delete.js - implement function that deletes file fileToRemove.txt 
+// (if there's no file fileToRemove.txt 
+// Error with message FS operation failed must be thrown)
+import { access, unlink } from 'node:fs/promises';
+
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const checkFile = async (file) => {
+  try {
+    await access(file);
+    return true;
+  } catch {
+    return false;
+  }
+};
+const removeFile = async (file) => {
+  let isFileExists = await checkFile(file);
+  if (!isFileExists) {
+    throw new Error(`*** FS operation failed. Not a such file : ${file}`);
+  }
+
+  await unlink(file);
+  console.log(`*** file ${file} has been removed`);
+};
 const remove = async () => {
-    // Write your code here 
+  const file = join(_dirname, 'files', 'fileToRemove.txt');
+  await removeFile(file).catch((err) => console.error(err.message));
 };
 
 await remove();
