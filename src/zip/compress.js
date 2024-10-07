e1c1f84d0a02d32c24ee2954b706c34b945bc504
olenaweb <olena.nevzorova@gmail.com>
2024-10-07 13:57:55 +0300

    feat: compress done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,37 @@
+import { access } from 'node:fs/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'node:zlib';
+
+import { dirname, join, basename } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const checkFile = async (file) => {
+  try {
+    await access(file);
+    return true;
+  } catch {
+    return false;
+  }
+};
+const compressFiles = async (archFile, zipFile) => {
+  let isFileExists = await checkFile(archFile);
+  if (!isFileExists) {
+    throw new Error(`*** Compress operation failed. Not a such file : ${archFile}`);
+  }
+  const read = createReadStream(archFile);
+  const gzip = createGzip();
+  const write = createWriteStream(zipFile);
+  read.pipe(gzip).pipe(write);
+  console.log(`*** File ${basename(archFile)} has been compressed to ${basename(zipFile)}`);
+}
+
 const compress = async () => {
-    // Write your code here 
+  const archFile = join(__dirname, 'files', 'fileToCompress.txt');
+  const zipFile = join(__dirname, 'files', 'archive.gz');
+  await compressFiles(archFile, zipFile).catch((err) => console.error(err.message));
 };
 
 await compress();
